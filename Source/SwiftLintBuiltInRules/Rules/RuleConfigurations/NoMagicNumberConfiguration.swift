917991551e7b8c7fe586177a6205f9005dc1c9a7
Effie <effie.thedev@gmail.com>
2025-04-16 16:55:41 +0900

    Remove allowed numbers element and Add allowed artgurment labels element

--- Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/NoMagicNumberConfiguration.swift
+++ Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/NoMagicNumberConfiguration.swift
@@ -3,13 +3,13 @@ import SwiftLintCore
 struct NoMagicNumbersRuleConfiguration: RuleConfiguration {
     typealias Parent = NoMagicNumbersRule
 
-    private(set) var allowedNumbers: Set<Double> = []
     private(set) var allowedMethodNames: Set<String> = []
+    private(set) var allowedArgumentLabels: Set<String> = []
 
     var consoleDescription: String {
         [
-            "allowed_numbers: \(allowedNumbers.sorted())",
-            "allowed_method_names: \(allowedMethodNames.sorted())"
+            "allowed_method_names: \(allowedMethodNames.sorted())",
+            "allowed_argument_labels: \(allowedArgumentLabels.sorted())"
         ].joined(separator: ", ")
     }
 
@@ -18,16 +18,12 @@ struct NoMagicNumbersRuleConfiguration: RuleConfiguration {
             throw Issue.invalidConfiguration(ruleID: Parent.identifier)
         }
 
-        if let numbers = config["allowed_numbers"] as? [Double] {
-            allowedNumbers = Set(numbers)
-        }
-
-        if let ints = config["allowed_numbers"] as? [Int] {
-            allowedNumbers = Set(ints.map(Double.init))
-        }
-
         if let methodNames = config["allowed_method_names"] as? [String] {
             allowedMethodNames = Set(methodNames)
+        }
+
+        if let labels = config["allowed_argument_labels"] as? [String] {
+            allowedArgumentLabels = Set(labels)
         }
     }
 }
